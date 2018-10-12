dea95833acdfe02b201dd7456eff26dd1fba531e
Will Connell <wconnell93@gmail.com>
2018-10-12 10:28:20 -0700

    updated logs file loc

--- R/analyses/submit.hypergeom-test-prod.sh
+++ R/analyses/submit.hypergeom-test-prod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash                                                        #-- what is the language of this shell
 #                                                                  #-- Any line that starts with #$ is an instruction to SGE
 #$ -S /bin/bash                                                    #-- the shell for the job
-#$ -o ../out/logs                        #-- output directory (fill in)
-#$ -e ../out/logs                        #-- error directory (fill in)
+#$ -o /netapp/home/wconnell/ye/projects/scCD4/out/logs                        #-- output directory (fill in)
+#$ -e /netapp/home/wconnell/ye/projects/scCD4/out/logs                        #-- error directory (fill in)
 #$ -cwd                                                            #-- tell the job that it should start in your working directory
 #$ -r y                                                            #-- tell the system that if a job crashes, it should be restarted
 #$ -j y                                                            #-- tell the system that the STDERR and STDOUT should be joined
