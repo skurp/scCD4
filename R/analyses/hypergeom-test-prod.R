ec995f37173011ec316460450b424a55439b1805
Will Connell <wconnell93@gmail.com>
2018-10-15 15:39:04 -0700

    split script to isolate hypergeom p-value analysis

--- R/analyses/hypergeom-test-prod.R
+++ R/analyses/hypergeom-test-prod.R
@@ -106,12 +106,15 @@ dev.off()
 # histogram of nominal and adjusted p-values
 ggplot(final) +
   geom_histogram(aes(x = p.value), bins = 100) +
+  ggtitle("Nominal p-values") +
   ggsave(sprintf("%s/p-val.png", out.dir), width = 10, height = 10, units = 'in')
 ggplot(final) +
   geom_histogram(aes(x = p.adjust), bins = 100) +
+  ggtitle("Adjusted p-values") +
   ggsave(sprintf("%s/p-adj.png", out.dir), width = 10, height = 10, units = 'in')
 ggplot(final) +
   geom_histogram(aes(x = p.adjust_all), bins = 100) +
+  ggtitle("Adjusted_all p-values") +
   ggsave(sprintf("%s/p-adj_all.png", out.dir), width = 10, height = 10, units = 'in')
 
 # filter to examine distribution closely
@@ -119,80 +122,13 @@ filt.final <- final %>% filter(p.adjust < 1)
 ggplot(filt.final) +
   geom_histogram(aes(x = p.adjust), bins = 100) +
   xlim(0, 0.5) +
+  ggtitle("Adjusted p-values") +
   ggsave(sprintf("%s/p-adj-filt.png", out.dir), width = 10, height = 10, units = 'in')
 filt.final <- final %>% filter(p.adjust_all < 1)
 ggplot(filt.final) +
   geom_histogram(aes(x = p.adjust_all), bins = 100) +
   xlim(0, 0.5) +
+  ggtitle("Adjusted_all p-values") +
   ggsave(sprintf("%s/p-adj_all-filt.png", out.dir), width = 10, height = 10, units = 'in')
 
-# Power analysis
-# https://cran.r-project.org/web/packages/pwr/vignettes/pwr-vignette.html
-# determine appropriate effect size (conventional estimations...)
-#
-
-# take filter set
-final.filt <- final %>%
-  filter(p.adjust2 <= 0.01)
-
-# function
-# to extract features unique to each cluster
-# as compared to all other clusters
-not_shared_feats <- function(hypergeom.analysis, feat) {
-  uq.feat <- list()
-  for( i in sort(unique(hypergeom.analysis$louvain))) {
-    not.clust.feat <- hypergeom.analysis %>%
-      filter(louvain != i) %>%
-      select(feat) %>%
-      distinct()
-    clust.uq.feat <- hypergeom.analysis %>%
-      filter(louvain == i) %>%
-      select(feat) %>%
-      distinct() %>%
-      anti_join(not.clust.feat)
-    colnames(clust.uq.feat) <- sprintf('%i', i)
-    uq.feat <- append(uq.feat, clust.uq.feat)
-  }
-  # corece to dataframe
-  lapply(uq.feat,`length<-`, max(lengths(uq.feat))) %>%
-    as.data.frame()
-}
-
-# which genes are not shared between clusters?
-uq.ge <- not_shared_feats(final.filt, "gene")
-# write out
-write_csv(uq.ge, sprintf('%s/KO_sigpos_uq-gene.csv', out.dir))
-
-# which guides are not shared between clusters?
-uq.cl <- not_shared_feats(final.filt, "guide")
-# write out
-write_csv(uq.cl, sprintf('%s/KO_sigpos_uq-guide.csv', out.dir))
-
-print("Number of guides in each cluster:")
-final.filt %>%
-  count(louvain, guide) %>%
-  print()
-print("Number of genes in each cluster:")
-final.filt %>%
-  count(louvain) %>%
-  print()
-
-# visualize no. genes associate w/ each guide in each cluster
-print(paste("Number of distinct genes associate with all guides:",
-            n_distinct(guide_data$gene)) ) # for reference
-# why is this one guide missing??
-print(
-  cell_meta$guide_cov[which(!(unique(cell_meta$guide_cov) %in% unique(guide_data$guide)))]
-  )
-
-clust_guide <- final.filt %>%
-  count(louvain, guide)
-ggplot(clust_guide) +
-  geom_bar(aes(x = guide, y = nn), stat = 'identity') +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  ggtitle("Sig-Pos KO Guide-Gene Freq") +
-  labs(x = "Guide", y = "No. Genes") +
-  coord_flip() +
-  facet_wrap(vars(louvain), nrow = 1) +
-  ggsave(sprintf('%s/KO_sigpos_freq.png', out.dir), width = 30, height = 20, units = 'in')
 
