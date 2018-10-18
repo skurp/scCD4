de3ce8e9448c4215146c71aaa281d8ab474dffd5
Will Connell <wconnell93@gmail.com>
2018-10-18 11:03:53 -0700

    testing script

--- analyses/hypergeom-test.R
+++ analyses/hypergeom-test.R
@@ -2,6 +2,7 @@
 # 2018.10.11
 
 # Depends -----------------------------------------------------------------
+devtools::load_all()
 library(readr)
 library(dplyr)
 library(purrr)
@@ -20,18 +21,18 @@ dir.create("../out/logs")
 # Input data --------------------------------------------------------------
 # all cell-guide - cluster
 path_cell_meta <- "~/ye/projects/scanpy/KO_cells.csv"
-cell_meta <- read_csv(path_cell_meta) %>%  sample_frac(0.1)
+cell_meta <- read_csv(path_cell_meta) %>% sample_frac(0.05)
 
 # all guide-gene combos
 path_guide_data <- "/ye/yelabstore2/dosageTF/tfko_140/combined/nsnp20.raw.sng.guide_sng.norm.vs0.igtb.guide.de.seurate.txt.meta.guide.meta.txt"
 guide_data <- read_tsv(path_guide_data) %>%
   rename(guide = cluster) %>%
-  select(guide, gene) %>% sample_frac(0.1)
+  select(guide, gene) %>% sample_frac(0.05)
 
 # all cluster-gene combos
 gene_loc_full <- "/ye/yelabstore2/dosageTF/tfko_140/combined/nsnp20.raw.sng.guide_sng.norm.igtb.louvain.de.seurate.meta.louvain.txt"
 gene_data_full <- read_tsv(gene_loc_full) %>%
-  select(cluster, gene) %>% sample_frac(0.1)
+  select(cluster, gene) %>% sample_frac(0.05)
 
 # intersect DFs
 # K, size of sample, total unique gene-guide combos
@@ -55,51 +56,45 @@ q <- cell_meta %>%
   count(louvain, guide, gene)
 
 # calculate
-final <- hypergeom_test(cell_meta)
-# final$p.adjust_all <- p.adjust(final$p.value, method = 'fdr')
+final <- hypergeom_test(cell_meta, q, m, K, n)
 # write out csv of guide-gene associated p.vals
-# write_csv(final, sprintf('%s/KO_sigpos_p-vals.csv', out.dir) )
+write_csv(final, sprintf('%s/KO_p-vals.csv', out.dir) )
 
 # Q-Q plot of p-values
-png(sprintf("%s/qq-test.png", out.dir), width = 8, height = 9, units = 'in', res = 200)
-plot( x = -log10(ppoints(length(final$p.value))),
-      y = -log10(sort(final$p.value)),
-      xlab= "Expected (-log10)",
-      ylab="Observed (-log10)" )
-abline(0,1,lty=45)
-dev.off()
+# png(sprintf("%s/qq-test.png", out.dir), width = 8, height = 9, units = 'in', res = 200)
+# plot( x = -log10(ppoints(length(final$p.value))),
+#       y = -log10(sort(final$p.value)),
+#       xlab= "Expected (-log10)",
+#       ylab="Observed (-log10)" )
+# abline(0,1,lty=45)
+# dev.off()
 
 # histogram of nominal and adjusted p-values
-melt.final <- final.vals %>%
+# histogram of nominal and adjusted p-values
+melt.final <- final %>%
   melt(measure.vars = c("louvain"),
        id.vars = c("p_value_enrich", "p_value_deplete", "p_adj_enrich", "p_adj_deplete"))
+# enriched test
 ggplot(melt.final) +
-  geom_histogram(aes(x =  p.value.enrich), bins = 100) +
+  geom_histogram(aes(x =  p_value_enrich), bins = 100) +
   facet_wrap(vars(value), nrow = 9, scales = "free") +
   ggsave(sprintf("%s/p-val-enrich.png", out.dir), width = 15, height = 30, units = 'in')
+# depleted test
+ggplot(melt.final) +
+  geom_histogram(aes(x =  p_value_deplete), bins = 100) +
+  facet_wrap(vars(value), nrow = 9, scales = "free") +
+  ggsave(sprintf("%s/p-val-deplete.png", out.dir), width = 15, height = 30, units = 'in')
+# FDR enriched test
+ggplot(melt.final) +
+  geom_histogram(aes(x =  p_adj_enrich), bins = 100) +
+  facet_wrap(vars(value), nrow = 9, scales = "free") +
+  ggsave(sprintf("%s/q-val-enrich.png", out.dir), width = 15, height = 30, units = 'in')
+# FDR depleted test
+ggplot(melt.final) +
+  geom_histogram(aes(x =  p_adj_deplete), bins = 100) +
+  facet_wrap(vars(value), nrow = 9, scales = "free") +
+  ggsave(sprintf("%s/q-val-deplete.png", out.dir), width = 15, height = 30, units = 'in')
 
-
-ggplot(final) +
-  geom_histogram(aes(x = p.value), bins = 100) +
-  ggsave(sprintf("%s/p-val.png", out.dir), width = 10, height = 10, units = 'in')
-ggplot(final) +
-  geom_histogram(aes(x = p.adjust), bins = 100) +
-  ggsave(sprintf("%s/p-adj.png", out.dir), width = 10, height = 10, units = 'in')
-ggplot(final) +
-  geom_histogram(aes(x = p.adjust_all), bins = 100) +
-  ggsave(sprintf("%s/p-adj_all.png", out.dir), width = 10, height = 10, units = 'in')
-
-# filter to examine distribution closely
-filt.final <- final %>% filter(p.adjust < 1)
-ggplot(filt.final) +
-  geom_histogram(aes(x = p.adjust), bins = 100) +
-  xlim(0, 0.5) +
-  ggsave(sprintf("%s/p-adj-filt.png", out.dir), width = 10, height = 10, units = 'in')
-filt.final <- final %>% filter(p.adjust_all < 1)
-ggplot(filt.final) +
-  geom_histogram(aes(x = p.adjust_all), bins = 100) +
-  xlim(0, 0.5) +
-  ggsave(sprintf("%s/p-adj_all-filt.png", out.dir), width = 10, height = 10, units = 'in')
 
 
 # Power analysis
