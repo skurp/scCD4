11b876d6785e428fb6c561dfaf667f26edb149fd
Will Connell <wconnell93@gmail.com>
2018-11-02 08:00:12 -0700

    tmp qsub time change

--- analyses/monocle_production.sh
+++ analyses/monocle_production.sh
@@ -9,14 +9,14 @@
 #$ -l mem_free=128G                  #-- submits on nodes with enough free memory (required)
 #$ -l arch=linux-x64               #-- SGE resources (CPU type)
 #$ -l netapp=25G,scratch=1G         #-- SGE resources (home and scratch disks)
-#$ -l h_rt=10:00:00                #-- runtime limit (see above; this requests 24 hours)
+#$ -l h_rt=4:00:00                #-- runtime limit (see above; this requests 24 hours)
 #$ -M wconnell93@gmail.com
 #$ -m bea
 
 date
 hostname
 
-qstat -j $JOB_ID 
+qstat -j $JOB_ID
 
 echo $input
 Rscript monocle_production.R $input
